6f309f04ed156b2a30eb050ea24a37e7b26cb8dd
RDeveloper123 <vasimakramc@gmail.com>
2024-03-13 18:13:11 +0530

    Calculater

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Calculater
+
+Technical skills involved in completing this project include:
+
+- HTML for structuring the webpage
+- CSS for styling the elements and making the interface visually appealing
+- JavaScript for implementing the calculator's functionality, handling user interactions, and performing calculations
+- Event handling to capture user input and trigger appropriate actions
+- Arithmetic operations and error handling to ensure accurate calculation results and prevent unexpected behavior
+- DOM manipulation to update the display dynamically based on user input and calculation results.
---
+++ script.js
@@ -0,0 +1,27 @@
+let string="";
+
+let buttons=document.querySelectorAll('.button');
+buttons.forEach((button)=>{
+    
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerHTML=='=')
+        {
+            
+            string=eval(string);
+            document.querySelector('input').value=string;
+
+        }
+        else if(e.target.innerHTML=='AC'){
+                string="";
+                document.querySelector('input').value=string;
+
+        }
+        else
+        {
+     
+        string=string+e.target.innerHTML;
+        document.querySelector('input').value=string;
+        }
+    })
+
+})
